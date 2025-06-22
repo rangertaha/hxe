7a972153c8efbdfe2297aaed6a7cf0a44041369d
rangertaha <rangertaha@gmail.com>
2025-06-22 01:26:53 -0400

    updates

--- pkg/client/programs.go
+++ pkg/client/programs.go
@@ -6,43 +6,47 @@ import (
 	"github.com/rangertaha/hxe/internal/models"
 )
 
-// Service operations
-func (c *Client) List() ([]models.Program, error) {
-	var services []models.Program
-	err := c.Get("/api/services", &services)
-	return services, err
+type ProgramClient struct {
+	GenericClient
 }
 
-func (c *Client) Get(id uint) (*models.Program, error) {
-	var service models.Program
-	err := c.Get(fmt.Sprintf("/api/services/%d", id), &service)
-	return &service, err
+// Program operations
+func (c *Client) ListPrograms() ([]models.Program, error) {
+	var programs []models.Program
+	err := c.Get("/api/program", &programs)
+	return programs, err
 }
 
-func (c *Client) Create(service *models.Program) (*models.Program, error) {
+func (c *Client) GetProgram(id uint) (*models.Program, error) {
+	var program models.Program
+	err := c.Get(fmt.Sprintf("/api/program/%d", id), &program)
+	return &program, err
+}
+
+func (c *Client) CreateProgram(program *models.Program) (*models.Program, error) {
 	var created models.Program
-	err := c.Post("/api/services", service, &created)
+	err := c.Post("/api/program", program, &created)
 	return &created, err
 }
 
-func (c *Client) Update(id uint, service *models.Program) (*models.Program, error) {
+func (c *Client) UpdateProgram(id uint, program *models.Program) (*models.Program, error) {
 	var updated models.Program
-	err := c.Put(fmt.Sprintf("/api/services/%d", id), service, &updated)
+	err := c.Put(fmt.Sprintf("/api/program/%d", id), program, &updated)
 	return &updated, err
 }
 
-func (c *Client) Delete(id uint) error {
-	return c.Delete(fmt.Sprintf("/api/services/%d", id))
+func (c *Client) DeleteProgram(id uint) error {
+	return c.Delete(fmt.Sprintf("/api/program/%d", id))
 }
 
-func (c *Client) Stop(id uint) error {
-	return c.Post(fmt.Sprintf("/api/services/%d/stop", id), nil, nil)
+func (c *Client) StopProgram(id uint) error {
+	return c.Post(fmt.Sprintf("/api/program/%d/stop", id), nil, nil)
 }
 
-func (c *Client) Start(id uint) error {
-	return c.Post(fmt.Sprintf("/api/services/%d/start", id), nil, nil)
+func (c *Client) StartProgram(id uint) error {
+	return c.Post(fmt.Sprintf("/api/program/%d/start", id), nil, nil)
 }
 
-func (c *Client) Restart(id uint) error {
-	return c.Post(fmt.Sprintf("/api/services/%d/restart", id), nil, nil)
+func (c *Client) RestartProgram(id uint) error {
+	return c.Post(fmt.Sprintf("/api/program/%d/restart", id), nil, nil)
 }
